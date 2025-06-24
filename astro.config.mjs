680c2d277121d9def7cce8029c74a76106e6b9b7
Jakub Hlusička <Limeth@users.noreply.github.com>
2025-06-25 00:38:10 +0200

    Add deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://limeth.github.io',
+  base: '/pin-calendar',
   integrations: [react(), vue()],
   vite: {
     plugins: [wasm(), topLevelAwait(), tailwindcss()],
