db3738bd7775263988b830ff1fcb6161cc88eda7
Jakub Hlusička <Limeth@users.noreply.github.com>
2025-07-13 01:06:58 +0200

    Fix drawer overlay in settings not being drawn over elements

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -8,14 +8,15 @@ const app = defineModel<App>();
 
 <template>
   <div class="join join-vertical w-full">
-    <div class="join-item collapse collapse-arrow bg-base-200">
+    <!-- `!isolation-auto` is used to prevent `collapse` from applying `isolation: isolate` and causing a new stacking context to be created, messing with the drawer overlay. -->
+    <div class="!isolation-auto join-item collapse collapse-arrow bg-base-200">
       <input type="radio" name="my-accordion-2" />
       <div class="collapse-title text-xl font-medium">Edit Pins</div>
       <div class="collapse-content">
         <SettingsPins v-model="app" />
       </div>
     </div>
-    <div class="join-item collapse collapse-arrow bg-base-200">
+    <div class="!isolation-auto join-item collapse collapse-arrow bg-base-200">
       <input type="radio" name="my-accordion-2" />
       <div class="collapse-title text-xl font-medium">Import/Export Settings</div>
       <div class="collapse-content">
