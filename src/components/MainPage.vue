7712916366cb1240e4c7609172876df504d2f3a6
Jakub Hlusička <Limeth@users.noreply.github.com>
2025-07-13 00:22:59 +0200

    Remove extraneous html tag

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -3,22 +3,8 @@ import SinglePageApp from './SinglePageApp.vue';
 </script>
 
 <template>
-  <!-- <!doctype html> -->
-  <html lang="en" data-theme="light">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="description" content="Astro description" />
-      <meta name="viewport" content="viewport-fit=cover, width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      <meta name="generator" content="{Astro.generator}" />
-      <title>{title}</title>
-    </head>
-
-    <body>
-      <Suspense>
-        <SinglePageApp />
-        <template #fallback> Loading </template>
-      </Suspense>
-    </body>
-  </html>
+  <Suspense>
+    <SinglePageApp />
+    <template #fallback><!-- TODO: Improve -->Loading</template>
+  </Suspense>
 </template>
