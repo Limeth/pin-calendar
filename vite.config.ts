eaa3064ae296752b0ea5bf878ef53c39aa874fd5
Jakub Hlusička <Limeth@users.noreply.github.com>
2025-07-20 13:19:05 +0200

    WIP: PWA

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA as vitePwa } from 'vite-plugin-pwa';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,7 +18,37 @@ export default defineConfig({
   },
   root: 'src',
   base: '/pin-calendar/',
-  plugins: [vue(), vueDevTools(), wasm(), topLevelAwait(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    wasm(),
+    topLevelAwait(),
+    tailwindcss(),
+    mkcert(),
+    vitePwa({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Pin Calendar',
+        short_name: 'Pin Cal',
+        description: 'A calendar-based activity tracking web app with multi-client synchronization',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'public/favicon.ico',
+            sizes: '32x32',
+            type: 'image/ico',
+          }
+        ]
+      },
+      workbox: {
+        sourcemap: true,
+      },
+      devOptions: {
+        resolveTempFolder: () => 'dev-dist',
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
